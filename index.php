4aa637df237807df542b90dd423def8798fe74ad
affa016 <affamatae@gmail.com>
2025-12-15 14:05:43 +0700

    Update index.php

--- index.php
+++ index.php
@@ -9,6 +9,11 @@
 	} else {
 		$uri = 'http://';
 	}
+if (!empty($_SERVER['HTTPS']) && ('on' == $_SERVER['HTTPS'])) {
+		$uri = 'https://';
+	} else {
+		$uri = 'http://';
+}
 	$uri .= $_SERVER['HTTP_HOST'];
 	header('Location: '.$uri.'/dashboard/');
 	exit;
